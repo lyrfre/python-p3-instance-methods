67c91cbebb804d1a9fca745d193f03fda9e14c1d
Lyra Freund <lyrafreund@gmail.com>
2023-10-24 19:17:01 -0600

    completed lab

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
+    
+    def talk(self):
+        print("Hello World!")
 
-    #Instance method definition
-    pass
+    def walk(self):
+        print("The person is walking.")
+
+
+lyra = Person()
+lyra.talk()
+lyra.walk()
+
+derek = Person()
+derek.talk()
+derek.walk()
